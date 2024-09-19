13f83ce5917723fd91f51a8634f56c4c2443b15e
Alex Rivard <alex.rivard4406@gmail.com>
2024-09-19 14:54:42 -0500

    database.sql code updated

--- database.sql
+++ database.sql
@@ -8,10 +8,12 @@ CREATE TABLE items (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(80) NOT NULL,
     "quantity" DECIMAL(6, 2),
-    "unit" VARCHAR(20)
+    "unit" VARCHAR(20),
+    "pricePerUnit" DECIMAL(8,2) DEFAULT '0',
+    "isPurchased" BOOLEAN DEFAULT FALSE
 );
 
-INSERT INTO items ("name", "quantity", "unit")
-VALUES ('Milk', '2', 'gallons'), ('Eggs', '12', 'cartons'), ('Juice', '1', 'bottle'), ('Cookies', '4', 'packages');
+INSERT INTO items ("name", "quantity", "unit", "pricePerUnit")
+VALUES ('Milk', '2', 'gallons', '8.50'), ('Eggs', '12', 'cartons', '4.99'), ('Juice', '1', 'bottle', '6.00'), ('Cookies', '4', 'packages', '10.99');
 
 DROP TABLE items;
