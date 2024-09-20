b946aa2a5934b48c2640c1ff87f75eacde36d047
Alex Rivard <alex.rivard4406@gmail.com>
2024-09-20 14:33:24 -0500

    changes for deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,20 @@
 const pg = require('pg');
 
-const pool = new pg.Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'fs-react-shopping', 
-});
+console.log(process.env.DATABASE_URL);
+if (process.env.DATABASE_URL) {
+    console.log(`Using cloud database config (DATABASE_URL found)`);
+    pool = new pg.Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    });
+  } else {
+    console.log(`Using local database config (no DATABASE_URL found)`);
+  
+    pool = new pg.Pool({
+      host: 'localhost',
+      port: 5432,
+      database: 'fs-react-shopping',
+    });
+  }
 
 module.exports = pool;
